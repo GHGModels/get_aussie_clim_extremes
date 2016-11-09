56445085c7c80eb353382a0ae1d877980cc3cd62
mdekauwe <mdekauwe@gmail.com>
2016-11-10 10:00:38 +1100

    Added header file

---
+++ include/get_lt_aussie_tmax.h
@@ -0,0 +1,23 @@
+#ifndef GET_LT_AUSSIE_TMAX_H
+#define GET_LT_AUSSIE_TMAX_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <netcdf.h>
+
+#define FILE_NAME "eMAST_ANUClimate_day_tmax_v1m0_20000101.nc"
+#define ERRCODE 2
+#define ERR(e) {printf("Error: %s\n", nc_strerror(e)); exit(ERRCODE);}
+#define NTIM 1
+#define NLAT 3474
+#define NLON 4110
+
+#define STRING_LENGTH 2000
+#define TRUE 1
+#define FALSE 0
+
+#define MIN(a,b) (((a) < (b)) ? (a) : (b))
+#define MAX(a,b) (((a) > (b)) ? (a) : (b))
+
+
+#endif /* GET_LT_AUSSIE_TMAX_H */
